1b0fe29aad773227d8df1f61135b0266979b2191
rafi nizar <rafi.nizar15@gmail.com>
2021-01-07 13:56:39 +0700

    added: register tutor

--- register/tutor/index.php
+++ register/tutor/index.php
@@ -1,4 +1,4 @@
-
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -46,96 +46,39 @@
 
 <body>
 
-    <!-- Navigation -->
-    <nav id="navorange" class="navbar fixed-top navbar-expand-lg fixed-top">
-        <div class="container">
-            <a class="navbar-brand" href="/CariPrivatYuk-PWEB/"><span id="jd1">Cari</span><span
-                    id="jd2">Privat</span><span id="jd3">Yuk!</span></a>
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <!-- Form cari nama tutor -->
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-primary my-2 my-sm-0" type="submit">Cari</button>
-                </form>
-
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link" href="/CariPrivatYuk-PWEB">Home</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">
-                            Privat
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
-                            <a class="dropdown-item" href="#">Akademik</a>
-                            <a class="dropdown-item" href="#">Development</a>
-                            <a class="dropdown-item" href="#">Bisnis</a>
-                            <a class="dropdown-item" href="#">Finansial</a>
-                            <a class="dropdown-item" href="#">Teknologi dan Software</a>
-                            <a class="dropdown-item" href="#">Desain</a>
-                            <a class="dropdown-item" href="#">Olahraga</a>
-                            <a class="dropdown-item" href="#">Seni</a>
-                            <a class="dropdown-item" href="#">Musik</a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                         <a href="/CariPrivatYuk-PWEB/login" class="nav-link dropdown-toggle" id="navbarDropdownBlog">Login <span class="caret"></span></a>
-                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
-                            <a class="dropdown-item" href="/CariPrivatYuk-PWEB/login/user">User</a>
-                            <a class="dropdown-item" href="/CariPrivatYuk-PWEB/login/tutor">Tutor</a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                         <a href="/CariPrivatYuk-PWEB/register" class="nav-link dropdown-toggle" id="navbarDropdownBlog">Register <span class="caret"></span></a>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
-                            <a class="dropdown-item" href="/CariPrivatYuk-PWEB/register/user">User</a>
-                            <a class="dropdown-item active" href="/CariPrivatYuk-PWEB/register/tutor">Tutor</a>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    
-    
-        
+    <?php include('../../partials/navbars/nav-orange.php'); ?>  
     
     <div class="container py-5 mt-5" id="register-area">
     <div class="row register d-flex justify-content-center">
         <div class="col-lg-4 parent-div">
             <div class="row child-div">
                 <div class="col-12">
+                    <?php require('../../partials/flash_messages/flash.php'); ?>
                     <h3 id="daftar" style="font-weight: bold;">Daftar Tutor</h3>
-                    <form>
+                    <form method="POST" action="/CariPrivatYuk-PWEB/controller/registerTutor.php" enctype="multipart/form-data">
                         <div class="form-group">
-                            <label for="username1">Nama Lengkap</label>
-                            <input type="text" class="form-control" name="username1" required>
+                            <label for="name_tutor">Nama Lengkap</label>
+                            <input type="text" class="form-control" name="name_tutor" required>
                         </div>
                         <div class="form-group">
-                            <label for="alamat1">Alamat</label>
-                            <input type="text" class="form-control" name="alamat1" required>
+                            <label for="address_tutor">Alamat</label>
+                            <input type="text" class="form-control" name="address_tutor" required>
                         </div>
                         <div class="form-group">
-                            <label for="email2">Email</label>
-                            <input type="email" class="form-control" name="email2" required>
+                            <label for="email_tutor">Email</label>
+                            <input type="email" class="form-control" name="email_tutor" required>
                         </div>
                         <div class="form-group">
-                            <label for="password2">Password</label>
-                            <input type="password" class="form-control" name="password2" required>
+                            <label for="password_tutor">Password</label>
+                            <input type="password" class="form-control" name="password_tutor" required>
                         </div>
                         <div class="form-group">
                             <label for="BerkasKTP">Berkas KTP</label>
-                            <input type="file" class="form-control-file" id="BerkasKTP">
+                            <input type="file" class="form-control-file" id="BerkasKTP" name="BerkasKTP">
                         </div>
                         <div class="form-group">
                             <label for="Foto">Foto anda</label>
-                            <input type="file" class="form-control-file" id="Foto">
+                            <input type="file" class="form-control-file" id="Foto" name="foto">
                         </div>
                         <button type="submit" class="btn btn-primary">Daftar</button>
                     </form>
