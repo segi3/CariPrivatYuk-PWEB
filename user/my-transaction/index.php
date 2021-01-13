ad35ee48e4b977738253037548979f8848d01378
rafi nizar <rafi.nizar15@gmail.com>
2021-01-13 14:24:48 +0700

    updated: navbar di halaman my privat dan my transaksi

--- user/my-transaction/index.php
+++ user/my-transaction/index.php
@@ -1,4 +1,4 @@
-
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,15 +11,11 @@
 
 <title>CariPrivatYuk! - My Trasaction</title>
 
-
 <link href="../../assets/main_resources/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
 
 <link href="../../assets/main_resources/css/modern-business.css" rel="stylesheet">
 
-
 <link href="../../assets/main_resources/css/style.css" rel="stylesheet">
-
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
 
@@ -41,64 +37,7 @@
 </head>
 
 <body>
-
-<nav id="navorange" class="navbar fixed-top navbar-expand-lg fixed-top">
-        <div class="container">
-            <a class="navbar-brand" href="/"><span id="jd1">Cari</span><span id="jd2">Privat</span><span
-                    id="jd3">Yuk!</span></a>
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-primary my-2 my-sm-0" type="submit">Cari</button>
-                </form>
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link" href="/CariPrivatYuk-PWEB">Home</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">
-                            Privat
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
-                            <a class="dropdown-item" href="#">Akademik</a>
-                            <a class="dropdown-item" href="#">Development</a>
-                            <a class="dropdown-item" href="#">Bisnis</a>
-                            <a class="dropdown-item" href="#">Finansial</a>
-                            <a class="dropdown-item" href="#">Teknologi dan Software</a>
-                            <a class="dropdown-item" href="#">Desain</a>
-                            <a class="dropdown-item" href="#">Olahraga</a>
-                            <a class="dropdown-item" href="#">Seni</a>
-                            <a class="dropdown-item" href="#">Musik</a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <!-- <a class="nav-link dropdown-toggle" href="user/" id="navbarDropdownPages" data-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">
-                            Rafi Nizar A.
-                        </a> -->
-                        <a href="/CariPrivatYuk-PWEB/user" class="nav-link dropdown-toggle"
-                            id="navbarDropdownBlog">Nama Pengguna <span class="caret"></span></a>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPages">
-                            <a class="dropdown-item" href="/CariPrivatYuk-PWEB/user/my-privat">My Privat</a>
-                            <a class="dropdown-item" href="/CariPrivatYuk-PWEB/user/my-transaction">My Transaction</a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link" href="#">Logout</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    
-    
-        
+    <?php include('../../partials/navbars/nav-orange.php'); ?>
     
     <div class="pt-5" style="background-color: #F3F3F3; height: 1000px;">
     <div class="container">
